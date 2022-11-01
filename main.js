d0114385d4467717e89eca38de8c8e25d6c9dfe0
3meraldK <ooyaxcontact@gmail.com>
2022-11-01 11:21:45 +0100

    update

--- main.js
+++ main.js
@@ -1,69 +1,70 @@
-String.prototype.format = function() {
-    a = this;
-    for (k in arguments) { a = a.replace("{" + k + "}", arguments[k]) }
-    return a;
-}
-
 function isEmpty(value) {
-    if (value.value.length == 0) return true;
-    return false;
+	if (value.value.length == 0) return true;
+	return false;
 }
 
 function updateFormable() {
-    
-    var output = document.getElementById("output-text");
+	
+	let output = document.getElementById('output-text'),
+		name = document.getElementById('name'),
+		canform = document.getElementById('canform'),
+		required = document.getElementById('required'),
+		exclusive = document.getElementById('exclusive'),
+		buttonName = document.getElementById('button-name'),
+		buttonDesc = document.getElementById('button-desc'),
+		alertTitle = document.getElementById('alert-title'),
+		alertDesc = document.getElementById('alert-desc'),
+		alertButton = document.getElementById('alert-button'),
+		flagLink = document.getElementById('flag-link');
 
-    var name = document.getElementById("name");
-    var canform = document.getElementById("canform");
-    var required = document.getElementById("required");
-    var exclusive = document.getElementById("exclusive");
-    var buttonName = document.getElementById("button-name");
-    var buttonDesc = document.getElementById("button-desc");
-    var alertTitle = document.getElementById("alert-title");
-    var alertDesc = document.getElementById("alert-desc");
-    var alertButton = document.getElementById("alert-button");
-    var flagLink = document.getElementById("flag-link");
-
-    canformlist = "";
-    requiredlist = "";
-    exclusivelist = "";
-
-    canformlist = formListString(canform, canformlist);
-    requiredlist = formListString(required, requiredlist);
-    exclusivelist = formListString(exclusive, exclusivelist);
+	const canformlist = formListString(canform, ''),
+		requiredlist = formListString(required, ''),
+		exclusivelist = formListString(exclusive, '');
 
 
-    if (!isEmpty(name) && !isEmpty(canform) && !isEmpty(required) && !isEmpty(buttonName) && !isEmpty(buttonDesc)) {
+	if (!isEmpty(name) && !isEmpty(canform) && !isEmpty(required) && !isEmpty(buttonName) && !isEmpty(buttonDesc)) {
 
-        copyVisibility('visible');
+		copyVisibility('visible');
+		output.innerHTML = 
+		`{<br>&nbsp;&nbsp;&nbsp;&nbsp;FormableName = "${name.value}",
+		<br>&nbsp;&nbsp;&nbsp;&nbsp;CountriesCanForm = {${canformlist}},
+		<br>&nbsp;&nbsp;&nbsp;&nbsp;RequiredCountries = {${requiredlist}},
+		<br>&nbsp;&nbsp;&nbsp;&nbsp;ExclusiveFormables = {${exclusivelist}},
+		<br><br>&nbsp;&nbsp;&nbsp;&nbsp;FormableButton = {<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;ButtonName = "${buttonName.value}",
+		<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;ButtonDescription = "${buttonDesc.value}",
+		<br>&nbsp;&nbsp;&nbsp;&nbsp;},
+		<br><br>&nbsp;&nbsp;&nbsp;&nbsp;CustomAlert = {<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Title = "${isEmpty(alertTitle) ? '' : alertTitle.value}",
+		<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Desc = "${isEmpty(alertDesc) ? '' : alertDesc.value}",
+		<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Button = "${isEmpty(alertButton) ? '' : alertButton.value}",
+		<br>&nbsp;&nbsp;&nbsp;&nbsp;},<br>},${isEmpty(flagLink) ? '' : '<br><br>Flag URL: ' + flagLink.value}`;
 
-        output.innerHTML = "{<br>&nbsp;&nbsp;&nbsp;&nbsp;FormableName = \"{0}\",<br>&nbsp;&nbsp;&nbsp;&nbsp;CountriesCanForm = {{1}},<br>&nbsp;&nbsp;&nbsp;&nbsp;RequiredCountries = {{2}},<br>&nbsp;&nbsp;&nbsp;&nbsp;ExclusiveFormables = {{3}},<br><br>&nbsp;&nbsp;&nbsp;&nbsp;FormableButton = {<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;ButtonName = \"{4}\",<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;ButtonDescription = \"{5}\",<br>&nbsp;&nbsp;&nbsp;&nbsp;},<br><br>&nbsp;&nbsp;&nbsp;&nbsp;CustomAlert = {<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Title = \"{6}\",<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Desc = \"{7}\",<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Button = \"{8}\",<br>&nbsp;&nbsp;&nbsp;&nbsp;},<br>},{9}".format(name.value, canformlist, requiredlist, exclusivelist, buttonName.value, buttonDesc.value, isEmpty(alertTitle) ? "" : alertTitle.value, isEmpty(alertDesc) ? "" : alertDesc.value, isEmpty(alertButton) ? "" : alertButton.value, isEmpty(flagLink) ? "" : "<br><br>Flag link: " + flagLink.value);
+		const isAlertVisible = (!isEmpty(alertTitle) || !isEmpty(alertDesc) || !isEmpty(alertButton));
+		if (!isAlertVisible) output.innerHTML = output.innerHTML.replace(
+			output.innerHTML.substring(output.innerHTML.indexOf('<br><br>&nbsp;&nbsp;&nbsp;&nbsp;CustomAlert'), 
+			output.innerHTML.indexOf('<br>&nbsp;&nbsp;&nbsp;&nbsp;},<br>},') + 30), '');
 
-        isAlertVisible = (!isEmpty(alertTitle) || !isEmpty(alertDesc) || !isEmpty(alertButton));
-        if (!isAlertVisible) output.innerHTML = output.innerHTML.replace(output.innerHTML.substring(output.innerHTML.indexOf("<br><br>&nbsp;&nbsp;&nbsp;&nbsp;CustomAlert"), output.innerHTML.indexOf("<br>&nbsp;&nbsp;&nbsp;&nbsp;},<br>},") + 30), "");
-
-    } else {
-        output.innerHTML = "Fill out required labels to proceed (marked by asterisks). Labels in italic are optional.";
-        copyVisibility('hidden');
-    }
+	} else {
+		output.innerHTML = 'Fill out required labels to proceed (marked by asterisks). Labels in italic are optional.';
+		copyVisibility('hidden');
+	}
 }
 
 function formListString(input, newlist) {
-    for (nation of input.value.split(",")) newlist = "{0}, \"{1}\"".format(newlist, nation.trim());
-    return newlist.replace(", ", "");
+	for (const nation of input.value.split(',')) newlist = `${newlist}, "${nation.trim()}"`;
+	return newlist.replace(', ', '');
 }
 
 function copyOutput() {
-    var output = document.getElementById("output-text");
-    if (output.innerHTML.length < 87) return;
-    navigator.clipboard.writeText(output.innerText);
+	const output = document.getElementById('output-text');
+	if (output.innerHTML.length < 87) return;
+	navigator.clipboard.writeText(output.innerText);
 }
 
 function copyVisibility(text) {
-    document.getElementById("copy-output").style.visibility = text;
+	document.getElementById('copy-output').style.visibility = text;
 }
 
 window.onload = function() {
-    updateFormable();
-    document.getElementById("copy-output").addEventListener("click", copyOutput);
+	updateFormable();
+	document.getElementById('copy-output').addEventListener('click', copyOutput);
 };
