13b20ab0054e8baf49379e678a00ab139136d697
3meraldK <ooyaxcontact@gmail.com>
2022-11-28 00:15:45 +0100

    tiles+qol changes

--- main.js
+++ main.js
@@ -1,47 +1,49 @@
 function isEmpty(value) {
-	if (value.value.length == 0) return true;
+	if (value.value.replaceAll(' ', '').length == 0) return true;
 	return false;
 }
 
 function updateFormable() {
 	
-	let output = document.getElementById('output-text'),
+	const output = document.getElementById('output-text'),
 		name = document.getElementById('name'),
 		canform = document.getElementById('canform'),
 		required = document.getElementById('required'),
+		tiles = document.getElementById('tiles'),
 		exclusive = document.getElementById('exclusive'),
 		buttonName = document.getElementById('button-name'),
 		buttonDesc = document.getElementById('button-desc'),
 		alertTitle = document.getElementById('alert-title'),
 		alertDesc = document.getElementById('alert-desc'),
 		alertButton = document.getElementById('alert-button'),
-		flagLink = document.getElementById('flag-link');
+		stabilityGain = document.getElementById('stability-gain'),
+		flagLink = document.getElementById('flag-link'),
 
-	const canformlist = formListString(canform, ''),
+		canformlist = formListString(canform, ''),
 		requiredlist = formListString(required, ''),
+		tilelist = formListString(tiles, ''),
 		exclusivelist = formListString(exclusive, '');
 
 
 	if (!isEmpty(name) && !isEmpty(canform) && !isEmpty(required) && !isEmpty(buttonName) && !isEmpty(buttonDesc)) {
 
 		copyVisibility('visible');
+		const isAlertVisible = (!isEmpty(alertTitle) || !isEmpty(alertDesc) || !isEmpty(alertButton));
 		output.innerHTML = 
 		`{<br>&nbsp;&nbsp;&nbsp;&nbsp;FormableName = "${name.value}",
 		<br>&nbsp;&nbsp;&nbsp;&nbsp;CountriesCanForm = {${canformlist}},
 		<br>&nbsp;&nbsp;&nbsp;&nbsp;RequiredCountries = {${requiredlist}},
-		<br>&nbsp;&nbsp;&nbsp;&nbsp;ExclusiveFormables = {${exclusivelist}},
+		${isEmpty(tiles) ? '' : `<br>&nbsp;&nbsp;&nbsp;&nbsp;RequiredTiles = {${tilelist}},`}
+		${isEmpty(exclusive) ? '' : `<br>&nbsp;&nbsp;&nbsp;&nbsp;ExclusiveFormables = {${exclusivelist}},`}
 		<br><br>&nbsp;&nbsp;&nbsp;&nbsp;FormableButton = {<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;ButtonName = "${buttonName.value}",
 		<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;ButtonDescription = "${buttonDesc.value}",
 		<br>&nbsp;&nbsp;&nbsp;&nbsp;},
-		<br><br>&nbsp;&nbsp;&nbsp;&nbsp;CustomAlert = {<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Title = "${isEmpty(alertTitle) ? '' : alertTitle.value}",
+		${!isAlertVisible ? '' : `<br><br>&nbsp;&nbsp;&nbsp;&nbsp;CustomAlert = {<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Title = "${isEmpty(alertTitle) ? '' : alertTitle.value}",
 		<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Desc = "${isEmpty(alertDesc) ? '' : alertDesc.value}",
-		<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Button = "${isEmpty(alertButton) ? '' : alertButton.value}",
-		<br>&nbsp;&nbsp;&nbsp;&nbsp;},<br>},${isEmpty(flagLink) ? '' : '<br><br>Flag URL: ' + flagLink.value}`;
-
-		const isAlertVisible = (!isEmpty(alertTitle) || !isEmpty(alertDesc) || !isEmpty(alertButton));
-		if (!isAlertVisible) output.innerHTML = output.innerHTML.replace(
-			output.innerHTML.substring(output.innerHTML.indexOf('<br><br>&nbsp;&nbsp;&nbsp;&nbsp;CustomAlert'), 
-			output.innerHTML.indexOf('<br>&nbsp;&nbsp;&nbsp;&nbsp;},<br>},') + 30), '');
+		<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Button = "${isEmpty(alertButton) ? '' : alertButton.value}",<br>&nbsp;&nbsp;&nbsp;&nbsp;},`}
+		${isEmpty(stabilityGain) ? '' : `<br><br>&nbsp;&nbsp;&nbsp;&nbsp;CustomAttributes = {<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;["Stability_Gain"] = ${stabilityGain.value},
+		<br>&nbsp;&nbsp;&nbsp;&nbsp;},`}
+		<br>},${isEmpty(flagLink) ? '' : '<br><br>Flag URL: ' + flagLink.value}`;
 
 	} else {
 		output.innerHTML = 'Fill out required labels to proceed (marked by asterisks). Labels in italic are optional.';
